229ab405d5fcdcf92e10aa89669942b7f8a5547c
raulmahya123 <raulmahya11@gmail.com>
2022-09-04 20:18:00 +0700

    first push

---
+++ krypt/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import { AiFillAlipayCircle } from "react-icons/ai";
+import {SiEthereum} from "react-icons/si"
+import {BsInfoCircle} from "react-icons/bs"
+
+const Welcome = () => {
+  return (
+    <div>Welcome</div>
+  )
+}
+
+export default Welcome;
