0d5325b484a10523fd70debe9df569a8969edcb6
raulmahya123 <raulmahya11@gmail.com>
2022-09-15 22:56:57 +0700

    mining jalan gelo

--- smart_contrak/hardhat.config.js
+++ smart_contrak/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle')
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity : '0.8.9',
+  networks: {
+    ropsten:{
+      url:'https://ropsten.infura.io/v3/645cd23a9fc9416c8b47cd6a82d6e02f',
+      accounts: ['77000044d5cf68a84d0ff7846fe98254391aa40b8270266f6c7f1dab8edcc59d']
+    }
+  }
+}
